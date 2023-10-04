a78fee6a60dd01858bfd013a25bd97b62b309318
Jeff Gonzalez <Jeff@hypertheory.com>
2023-10-04 08:00:21 -0400

    Added stubbed tests

---
+++ Api1.ContractTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Api1.ContractTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Api2.ContractTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Api2.ContractTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
